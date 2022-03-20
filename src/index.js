58ef47fca1339db656fc582bfb453451ede09234
gafoor8374 <97434962+gafoor8374@users.noreply.github.com>
2022-03-21 00:36:21 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const connect = require('./configs/db');
+const userController = require('./controllers/user.controller');
+const productController = require("./controllers/product.controller")
+
+const {register,login}= require('./controllers/auth.controller')
+
+// const user= require('./models/user.model')
+const app = express();
+
+app.use(express.json());
+
+app.use("/users",userController);
+
+app.post("/register",register);
+
+app.post("/login",login);
+app.use("/products", productController)
+
+
+app.listen(5000, async (req, res) => {
+    try{
+      await connect();
+      console.log('listening on port 5000')
+    }catch(err){
+        console.log(err.message);
+    }
+})
